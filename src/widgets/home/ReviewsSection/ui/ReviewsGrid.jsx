45b878f3b5d9532746a713dd77ca219803f636df
sxd993 <gamhotik2005@mail.ru>
2025-12-24 17:02:42 +0500

    гриды раскинуты

--- src/widgets/home/ReviewsSection/ui/ReviewsGrid.jsx
+++ src/widgets/home/ReviewsSection/ui/ReviewsGrid.jsx
@@ -3,7 +3,7 @@ import { ReviewCard } from "@/entities/marketing/review/ui/ReviewCard"
 
 export const ReviwsGrid = () => {
     return (
-        <div className="grid grid-cols-3 grid-rows-2 content-between gap-5 h-full w-full">
+        <div className="grid grid-cols-2 md:grid-cols-3 grid-rows-3 md:grid-rows-2 content-between gap-5 h-full w-full">
             {REVIEWS.slice(0,6).map((review) => (
                 <ReviewCard key={review.id} text={review.text} image={review.image} />
             ))}
--- src/widgets/home/ReviewsSection/ui/ReviewsSection.jsx
+++ src/widgets/home/ReviewsSection/ui/ReviewsSection.jsx
@@ -6,15 +6,15 @@ export const ReviewsSection = () => {
     return (
         <div className="flex flex-col gap-10 max-w-6xl px-[2%] lg:px-0 mx-auto">
             {/* Заголовок секции */}
-            <p className="text-primary text-5xl">
+            <p className="text-primary text-3xl">
                 Отзывы о нас
             </p>
             {/* Отзывы */}
             <div className="flex w-full justify gap-5">
-                <div className="basis-1/3">
+                <div className="basis-4/6 md:basis-1/3 ">
                     <BigReview />
                 </div>
-                <div className="basis-2/3">
+                <div className="basis-3/6 md:basis-2/3">
                     <ReviwsGrid />
                 </div>
             </div>
